0fcc362c6e6128cb0d68d367797d9dbd395c27ba
Victor Garcia EDU <garciaveduar@gmail.com>
2024-02-09 16:21:43 -0400

    avnce de visual

--- src/pages/LevelUp/index.jsx
+++ src/pages/LevelUp/index.jsx
@@ -3,8 +3,9 @@ import Layout from "../../components/Layout";
 const LevelUp = () => {
   return (
     <Layout>
-      <div>
-        <p className="text-gray-100 text-6xl font-bold">LevelUp</p>
+      <div className="w-10/12 h-60 grid gap-6 place-content-center rounded-2xl  bg-purple-200 border-4 border-purple-600 ">
+        <p className="text-purple-800 text-6xl font-bold text-center">LevelUp</p>
+        <p className="text-xl text-center text-indigo-800">Haz respondido de manera correcta</p>
       </div>
     </Layout>
   );
